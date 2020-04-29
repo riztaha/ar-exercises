9324f0d7ddb89f1a6b748de8afa945eea5ec5c24
Taha Rizvi <riztaha@gmail.com>
2020-04-28 23:41:47 -0400

    Validations created for employee & store. Printing error msgs.

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -1,12 +1,45 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
-require_relative './exercise_3'
-require_relative './exercise_4'
-require_relative './exercise_5'
-require_relative './exercise_6'
+# 1. Add validations to two models to enforce the following business rules:
+#   * Employees must always have a first name present
+#   * Employees must always have a last name present
+#   * Employees have a hourly_rate that is a number (integer) between 40 and 200
+#   * Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
+#   * Stores must always have a name that is a minimum of 3 characters
+#   * Stores have an annual_revenue that is a number (integer) that must be 0 or more
+#   * BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a [custom validation method](http://guides.rubyonrails.org/active_record_validations.html#custom-methods) - **don't** use a `Validator` class)
+# 2. Ask the user for a store name (store it in a variable)
+# 3. Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
+# 4. Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+
+require_relative "../setup"
+require_relative "./exercise_1"
+require_relative "./exercise_2"
+require_relative "./exercise_3"
+require_relative "./exercise_4"
+require_relative "./exercise_5"
+require_relative "./exercise_6"
 
 puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee
+  validates :first_name, :last_name, :store_id, presence: true
+  validates :hourly_rate, inclusion: (40..200)
+end
+
+class Store
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+end
+
+# @store1.employees.create(first_name: "Dwayne", last_name: "", hourly_rate: 150).errors.full_messages
+# @store1.employees.create(first_name: "", last_name: "Johnson", hourly_rate: 150).errors.full_messages
+# @store1.employees.create(first_name: "Dwayne", last_name: "Johnson", hourly_rate: 150)
+
+puts "What is the name of your store?"
+store_name = gets.chomp
+@new_store = Store.create(name: store_name)
+errors = @new_store.errors.messages
+puts "Could not create store due to error ======> #{errors}"
+
+# Run in terminal with 'bundle exec ruby exercises/exercise_7.rb'
